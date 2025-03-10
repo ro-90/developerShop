8a27ea76ad00888a69d72090c0be2806c06c1628
rosacastaño <rosacastano378@gmail.com>
2025-03-10 00:13:04 -0300

    agregue cambios

---
+++ src/routes/login.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+let loginController = require('../controllers/loginController');
+
+
+
+router.get('/', loginController.index);
+
+
+    module.exports = router;
