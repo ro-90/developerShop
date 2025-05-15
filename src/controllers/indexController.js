53f301867e69f210bbe62e3d4d5c0eff01ebb71e
Eric <menaericdaniel@gmail.com>
2025-05-15 04:31:03 -0300

    fix model & show products home

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,18 +1,29 @@
-
-
-const fs = require('fs');
+const db = require('../database/models');
 
 const indexController = {
-    index: function (req, res, next) {
-
-        const archivoProductos = fs.readFileSync(__dirname + "/product.json", "utf-8");
-        const products = JSON.parse(archivoProductos);
-
-        return res.render('index', { title: 'DevelopShop', products });
+    index: async (req, res, next) =>  {
+        try {
+            const [dress, accessories] = await Promise.all([
+                db.Product.findAll({
+                    where: {
+                        categoryId: 1
+                    },
+                    order: db.sequelize.random(),
+                    limit: 4
+                }),
+                db.Product.findAll({
+                    where: {
+                        categoryId: 2
+                    },
+                    order: db.sequelize.random(),
+                    limit: 4
+                })
+            ]);
+            return res.render('index', { title: 'DevelopShop', dress, accessories });
+        } catch (error) {
+            console.log(error);
+        }
     }
-
-
 }
-
 
 module.exports = indexController;
